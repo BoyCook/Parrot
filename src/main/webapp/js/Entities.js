5949bbeeae988e85680a1c8761468da8f8493fe1
Craig Cook <boycook@me.com>
2012-07-30 14:35:47 +0100

    UI can view empty list of entities

--- src/main/webapp/js/Entities.js
+++ src/main/webapp/js/Entities.js
@@ -24,20 +24,24 @@ Entities.prototype.renderEntities = function(model, data) {
     html += "</tr>";
 
     //TODO: only render columns in the model
-    $.each(data['@items'], function (index, entity) {
-        html += "<tr>";
-        $.each(model.attributes['@items'], function(index, value){
-            if (value.column == true) {
-                var val = entity[value.name];
-                if (value.identity == true) {
-                    html += "<td><a href='/service/" + model.name + "/" + val + "' target='_blank'>" + val + "</a></td>"
-                } else {
-                    html += "<td>" + val + "</td>";
+
+    if (data['@items']) {
+        $.each(data['@items'], function (index, entity) {
+            html += "<tr>";
+            $.each(model.attributes['@items'], function(index, value){
+                if (value.column == true) {
+                    var val = entity[value.name];
+                    if (value.identity == true) {
+                        html += "<td><a href='/service/" + model.name + "/" + val + "' target='_blank'>" + val + "</a></td>"
+                    } else {
+                        html += "<td>" + val + "</td>";
+                    }
                 }
-            }
+            });
+            html += "</tr>";
         });
-        html += "</tr>";
-    });
+    }
+
     html += "</table>";
     $(context.entitiesId).html(html);
 };

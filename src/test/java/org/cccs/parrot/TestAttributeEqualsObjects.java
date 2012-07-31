e011e112b69485c0cfa24f7a206c9c818b31712e
Craig Cook <boycook@me.com>
2012-07-31 14:28:34 +0100

    Added attribute equals test

---
+++ src/test/java/org/cccs/parrot/TestAttributeEqualsObjects.java
@@ -0,0 +1,57 @@
+package org.cccs.parrot;
+
+import org.cccs.parrot.domain.Attribute;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * User: boycook
+ * Date: 31/07/2012
+ * Time: 14:20
+ */
+public class TestAttributeEqualsObjects {
+
+    private Attribute attribute = new Attribute("att1", "Att 1", Object.class);
+
+    @Test
+    public void attributeEqualsShouldWork() {
+        assertTrue(attribute.equals(new Attribute("att1", "Att 1", Object.class)));
+    }
+
+    @Test
+    public void attributeEqualsShouldBeFalseForNull() {
+        assertFalse(attribute.equals(null));
+    }
+
+    @Test
+    public void attributeEqualsShouldBeFalseForNullClass() {
+        assertFalse(attribute.equals(new Attribute("att1", "Att 1", null)));
+    }
+
+    @Test
+    public void attributeEqualsShouldBeFalseForNullDescription() {
+        assertFalse(attribute.equals(new Attribute("att1", null, Object.class)));
+    }
+
+    @Test
+    public void attributeEqualsShouldBeFalseForNullName() {
+        assertFalse(attribute.equals(new Attribute(null, "Att 1", Object.class)));
+    }
+
+    @Test
+    public void attributeEqualsShouldBeFalseForDifferentClass() {
+        assertFalse(attribute.equals(new Attribute("att1", "Att 1", String.class)));
+    }
+
+    @Test
+    public void attributeEqualsShouldBeFalseForDifferentDescription() {
+        assertFalse(attribute.equals(new Attribute("att1", "DIFFERENT", Object.class)));
+    }
+
+    @Test
+    public void attributeEqualsShouldBeFalseForDifferentName() {
+        assertFalse(attribute.equals(new Attribute("DIFFERENT", "Att 1", Object.class)));
+    }
+}
